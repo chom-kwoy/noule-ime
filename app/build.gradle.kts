5eac805b1c951fb7fb2d58f244ef2afd0f6175ac
Geon Park <mujjingun@gmail.com>
2025-10-23 23:39:55 +0900

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "org.chocassye.noule"
         minSdk = 24
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.2.0"
+        versionCode = 7
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
